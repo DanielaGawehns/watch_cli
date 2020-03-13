b9988b9e88510eacd51b78090805d9b9ee4bfd7e
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2020-03-13 13:11:50 +0100

    actually make live command do stuff

--- src/main/java/nl/liacs/watch_cli/commands/Live.java
+++ src/main/java/nl/liacs/watch_cli/commands/Live.java
@@ -1,13 +1,13 @@
 package nl.liacs.watch_cli.commands;
 
 import java.io.IOException;
-import java.util.concurrent.Executors;
 
-import nl.liacs.watch.protocol.server.WrappedConnection;
-import nl.liacs.watch.protocol.types.Message;
 import nl.liacs.watch.protocol.types.MessageParameterLong;
+import nl.liacs.watch_cli.WatchConnector;
 
 public class Live implements Command {
+    private boolean isRunning;
+
     public String getDescription() {
         return "Show a live view of all enabled sensors with the given interval of the given devices.\nDevice IDs are comma delimited.";
     }
@@ -20,36 +20,31 @@ public class Live implements Command {
         var deviceIds = args.getRest().get(0).split(",");
         var interval = Integer.parseInt(args.getRest().get(1));
 
-        WrappedConnection[] conns = new WrappedConnection[deviceIds.length];
+        WatchConnector[] connectors = new WatchConnector[deviceIds.length];
         for (int i = 0; i < deviceIds.length; i++) {
-            var conn =  Utils.getWatchConnection(deviceIds[i]);;
+            var connector = Utils.getConnector(deviceIds[i]);
 
-            if (conn == null) {
-                System.err.printf("watch with id '%s' not found or not connected\n", deviceIds[i]);
+            if (connector.isClosed()) {
+                System.err.printf("watch with id %s not connected\n", deviceIds[i]);
                 return;
             }
-            conns[i] = conn;
+
+            connectors[i] = connector;
 
             try {
-                conn.setValues("live.interval", new MessageParameterLong(interval));
+                connector.getConnection().setValues("live.interval", new MessageParameterLong(interval));
             } catch (IOException e) {
                 System.out.println(e.getStackTrace());
                 System.exit(1);
             }
         }
 
-        var pool = Executors.newFixedThreadPool(conns.length);
-        for (var conn : conns) {
-            pool.submit(() -> {
-                while (!conn.isClosed()) {
-                    Message msg;
-                    try {
-                        msg = conn.receive();
-                    } catch (InterruptedException e) {
-                        return;
-                    }
+        this.isRunning = true;
 
-                    System.out.println(msg.toString());
+        for (var connector : connectors) {
+            connector.addIncrementConsumer(point -> {
+                if (this.isRunning) {
+                    System.out.printf("%s\n", point);
                 }
             });
         }
@@ -61,6 +56,6 @@ public class Live implements Command {
             System.exit(1);
         }
 
-        pool.shutdownNow();
+        this.isRunning = false;
     }
 }
